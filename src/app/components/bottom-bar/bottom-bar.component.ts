23b43c1a8010c75cca471e6b0f4e126a18136820
4skinSkywalker <stopchemtrailsfred@gmail.com>
2021-07-28 12:55:59 +0200

    Add a dark-mode by using CSS filter

--- src/app/components/bottom-bar/bottom-bar.component.ts
+++ src/app/components/bottom-bar/bottom-bar.component.ts
@@ -7,6 +7,18 @@ import { Component } from '@angular/core';
 })
 export class BottomBarComponent {
 
+    darkMode = false;
+
     constructor() { }
+
+    toggleDarkMode() {
+        this.darkMode = !this.darkMode;
+        let html = document.querySelector("html");
+        if (this.darkMode) {
+            html.setAttribute("theme", "dark-mode");
+        } else {
+            html.setAttribute("theme", "light-mode");
+        }
+    }
 
 }
