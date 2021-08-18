3388239c8fccffae7dbbd085aac3840d0af784e6
4skinSkywalker <stopchemtrailsfred@gmail.com>
2021-08-18 18:14:34 +0200

    Save the show state of the console

--- src/app/components/view-console/view-console.component.ts
+++ src/app/components/view-console/view-console.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ViewConsoleService } from 'src/app/services/view-console.service';
 
 @Component({
@@ -8,13 +8,31 @@ import { ViewConsoleService } from 'src/app/services/view-console.service';
 })
 export class ViewConsoleComponent implements OnInit {
 
+    @ViewChild("consoleOpeningCheckbox") consoleOpeningCheckbox;
+
     messages;
+
+    consoleShowState = JSON.parse(localStorage.getItem("consoleShowState")) || false;
 
     constructor(private viewConsoleServ: ViewConsoleService) { }
 
     ngOnInit(): void {
         this.messages = this.viewConsoleServ.messages;
         // setInterval(() => this.viewConsoleServ.log("I like ice cream"), 1000);
+    }
+
+    ngAfterViewInit() {
+        // Show the console if the consoleShowState is true
+        if (this.consoleShowState) {
+            this.consoleOpeningCheckbox.nativeElement.checked = true;
+        }
+    }
+
+    setConsoleShowState(checkbox) {
+        localStorage.setItem(
+            "consoleShowState",
+            JSON.stringify(checkbox.checked)
+        );
     }
 
 }
