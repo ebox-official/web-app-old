fb83b657ce9becfbdc1301b6844ae4cbe116b0b0
4skinSkywalker <stopchemtrailsfred@gmail.com>
2021-08-18 15:53:33 +0200

    Make the console way less annoying and mobile-friendly

--- src/app/components/view-console/view-console.component.ts
+++ src/app/components/view-console/view-console.component.ts
@@ -15,23 +15,6 @@ export class ViewConsoleComponent implements OnInit {
     ngOnInit(): void {
         this.messages = this.viewConsoleServ.messages;
         // setInterval(() => this.viewConsoleServ.log("I like ice cream"), 1000);
-
-        var viewConsole: any = document.querySelector("#view-console");
-        var handle: any = document.querySelector("#view-console-handle");
-
-        // (1) When mouse is down on "handle" start the drag-and-drop
-        handle.addEventListener("mousedown", () => {
-
-            let onMouseMove = ev => viewConsole.style.height = window.innerHeight - ev.pageY + 'px'
-
-            // (2) Track the mouse movements on the entire document
-            document.addEventListener('mousemove', onMouseMove);
-
-            // (3) When mouse is up anywhere in the document remove "onMouseMove" from the listeners
-            document.addEventListener("mouseup", () => 
-                document.removeEventListener('mousemove', onMouseMove)
-            );
-        });
     }
 
 }
